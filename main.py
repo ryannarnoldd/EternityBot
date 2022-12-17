9472fd088f3866244a3e25254cafde08542f1b17
ryannarnoldd <ryannarnoldd@gmail.com>
2022-12-17 12:27:06 -0500

    Simple random roll command created

--- main.py
+++ main.py
@@ -1,13 +1,27 @@
 import discord
+from discord.ext import commands
 import os
 from dotenv import load_dotenv
+import random
 load_dotenv()
 
-client = discord.Client(intents=discord.Intents.all())
+bot = commands.Bot(command_prefix='!', intents=discord.Intents.all())
 
-@client.event
+# On ready event.
+@bot.event
 async def on_ready():
-    print(f'{client.user} has connected to Discord!')
+    print(f'Logged in as {bot.user}!')
 
+# Roll command.
+@bot.command(name='roll', brief='It will roll dice!', aliases=['dice','die','rolldie'])
+async def roll(ctx, max = '6'):
+    if max.isdigit() and int(max) > 0: result = f'1-{int(max)}:  **{random.randint(1,int(max))}**'
+    else: result = 'Please enter a valid number!'
 
-client.run(os.getenv('TOKEN'))
+    await ctx.send(embed = discord.Embed(
+        title = 'Dice Roll',
+        description = result, 
+        color = discord.Color.blue()
+    ))
+
+bot.run(os.getenv('TOKEN'))
