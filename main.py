a735d2341d19c622ee5f7477ca7894fa946e3d0a
ryannarnoldd <ryannarnoldd@gmail.com>
2022-12-18 22:45:36 -0500

    Created a greeting On message event

--- main.py
+++ main.py
@@ -5,12 +5,26 @@ from dotenv import load_dotenv
 import random
 load_dotenv()
 
+greetings = ['hello', 'hi', 'hey', 'yo', 'sup', 'wassup']
+
 bot = commands.Bot(command_prefix='!', intents=discord.Intents.all())
 
 # On ready event.
 @bot.event
 async def on_ready():
     print(f'Logged in as {bot.user}!')
+
+# On message event.
+@bot.event
+async def on_message(message):
+    if message.author == bot.user: return
+
+    # If the message contains any of the greetings, reply with a greeting!
+    if any(greet in message.content.lower().strip() for greet in greetings):
+        await message.reply('Hello!')
+
+    # Processes commands registered with the bot.
+    await bot.process_commands(message)
 
 # Roll command.
 @bot.command(name='roll', brief='It will roll dice!', aliases=['dice','die','rolldie'])
@@ -23,5 +37,7 @@ async def roll(ctx, max = '6'):
         description = result, 
         color = discord.Color.blue()
     ))
+
+
 
 bot.run(os.getenv('TOKEN'))
