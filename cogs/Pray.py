e380af5a1fd7430e8dda86d8da54ba342b1beae6
ryannarnoldd <ryannarnoldd@gmail.com>
2022-12-25 11:11:36 -0500

    Checking out parameters, descriptions, and else

--- cogs/Pray.py
+++ cogs/Pray.py
@@ -7,13 +7,22 @@ class Pray(commands.GroupCog, name="pray"):
         self.bot = bot
         super().__init__()
     
-    @app_commands.command(name="list")
+    @app_commands.command(name="list", description="List all prayers.")
     async def list(self, interaction: discord.Interaction) -> None:
         await interaction.response.send_message("Hello from list", ephemeral=True)
+
+    @app_commands.command(name="recent", description="List the erecent n prayers.")
+    async def recent(self, interaction: discord.Interaction, n: int = 5) -> None:
+        await interaction.response.send_message(f"Hello from recent {n}", ephemeral=True)
     
-    @app_commands.command(name="add")
-    async def add(self, interaction: discord.Interaction) -> None:
-        await interaction.response.send_message("Hello from add", ephemeral=True)
+    @app_commands.command(name="add", description="Add a prayer request.")
+    async def add(self, interaction: discord.Interaction, *, prayer: str) -> None:
+        await interaction.response.send_message(f"Hello from add {prayer}", ephemeral=True)
+
+    @app_commands.command(name="answer", description="Mark a prayer as answered. (Praise God!)")
+    async def answer(self, interaction: discord.Interaction, index: int) -> None:
+        await interaction.response.send_message(f"Hello from answer {index}", ephemeral=True)
+
     
 async def setup(bot: commands.Bot) -> None:
   await bot.add_cog(Pray(bot))
