eda94e5dd4acf85374aae0b3346081b877ea88a5
ryannarnoldd <ryannarnoldd@gmail.com>
2022-12-25 10:52:45 -0500

    Simple cog slash command after a long time working

--- Bot.py
+++ Bot.py
@@ -2,27 +2,37 @@ import asyncio
 import discord
 from discord.ext import commands
 import os
+from discord import app_commands
 from dotenv import load_dotenv
+from typing import Literal, Optional
+from discord.ext import commands
+from discord.ext.commands import Greedy, Context # or a subclass of yours
 load_dotenv()
 
-greetings = ['hello', 'hi', 'hey', 'yo', 'sup', 'wassup']
+GUILD = discord.Object(id=1045498586105913345)
 
-bot = commands.Bot(command_prefix='!', intents=discord.Intents.all())
-bot.remove_command('help')
+class Bot(commands.Bot):
+    GUILD = discord.Object(id=1045498586105913345)
+    def __init__(self):
+        super().__init__(command_prefix='!', intents=discord.Intents.all())
+        
+    async def setup_hook(self):
+        self.tree.copy_global_to(guild=GUILD)
+        await self.tree.sync(guild=GUILD)
 
+bot = Bot()
+    
 # On ready event.
 @bot.event
 async def on_ready():
     print(f'Logged in as {bot.user}!')
 
 # On message event.
+# Bot is not able to use commands.
 @bot.event
 async def on_message(message):
     if message.author == bot.user: return
-
-    # Processes commands registered with the bot.
-    await bot.process_commands(message)
-    
+    await bot.process_commands(message) 
 
 async def load_cogs():
     for filename in os.listdir('./cogs'):
